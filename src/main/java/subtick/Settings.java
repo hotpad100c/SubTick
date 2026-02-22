69549ccb2fedea21d4d119364d1816274c39b226
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-02-22 19:58:18 +0800

    补全翻译

--- src/main/java/subtick/Settings.java
+++ src/main/java/subtick/Settings.java
@@ -5,46 +5,45 @@ import carpet.settings.Rule;
 public class Settings
 {
   @Rule(
-    desc = "The default tick phase to freeze at and step to",
-    options = {"worldBorder", "weather", "time", "blockTick", "fluidTick", "raid", "chunk", "blockEvent", "entity", "blockEntity", "entityManagement"},
-    strict = true,
-    category = "subtick"
+          desc = "carpet.rule.subtickDefaultPhase.desc",
+          options = {"worldBorder", "weather", "time", "blockTick", "fluidTick", "raid", "chunk", "blockEvent", "entity", "blockEntity", "entityManagement"},
+          category = "subtick"
   )
   public static String subtickDefaultPhase = "blockTick";
 
   @Rule(
-    desc = "Text format for normal text in subtick message feedback (Uses carpet format, search for \"format(components, ...)\" in https://github.com/gnembon/fabric-carpet/blob/master/docs/scarpet/api/Auxiliary.md)",
-    category = "subtick"
+          desc = "carpet.rule.subtickTextFormat.desc",
+          category = "subtick"
   )
   public static String subtickTextFormat = "ig";
 
   @Rule(
-    desc = "Text format for numbers in subtick message feedback (Uses carpet format, search for \"format(components, ...)\" in https://github.com/gnembon/fabric-carpet/blob/master/docs/scarpet/api/Auxiliary.md)",
-    category = "subtick"
+          desc = "carpet.rule.subtickNumberFormat.desc",
+          category = "subtick"
   )
   public static String subtickNumberFormat = "iy";
 
   @Rule(
-    desc = "Text format for phases in subtick message feedback (Uses carpet format, search for \"format(components, ...)\" in https://github.com/gnembon/fabric-carpet/blob/master/docs/scarpet/api/Auxiliary.md)",
-    category = "subtick"
+          desc = "carpet.rule.subtickPhaseFormat.desc",
+          category = "subtick"
   )
   public static String subtickPhaseFormat = "it";
 
   @Rule(
-    desc = "Text format for dimensions in subtick message feedback (Uses carpet format, search for \"format(components, ...)\" in https://github.com/gnembon/fabric-carpet/blob/master/docs/scarpet/api/Auxiliary.md)",
-    category = "subtick"
+          desc = "carpet.rule.subtickDimensionFormat.desc",
+          category = "subtick"
   )
   public static String subtickDimensionFormat = "im";
 
   @Rule(
-    desc = "Error text format for normal text in subtick message feedback (Uses carpet format, search for \"format(components, ...)\" in https://github.com/gnembon/fabric-carpet/blob/master/docs/scarpet/api/Auxiliary.md)",
-    category = "subtick"
+          desc = "carpet.rule.subtickErrorFormat.desc",
+          category = "subtick"
   )
   public static String subtickErrorFormat = "ir";
 
   @Rule(
-    desc = "Default range for queueStep",
-    category = "subtick"
+          desc = "carpet.rule.subtickDefaultRange.desc",
+          category = "subtick"
   )
   public static int subtickDefaultRange = 32;
 }
