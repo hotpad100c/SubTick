20efabcb93739cacc81861b2df81f968be123173
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-02-27 22:38:55 +0800

    兼容Flashback

--- src/main/java/subtick/mixins/client/LevelRendererMixin.java
+++ src/main/java/subtick/mixins/client/LevelRendererMixin.java
@@ -1,5 +1,10 @@
 package subtick.mixins.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import subtick.client.ClientTickHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -40,6 +45,10 @@ import carpet.fakes.MinecraftClientInferface;
 //$$ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 //$$ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 //#endif
+//#if MC >= 12110
+//$$ import net.minecraft.client.renderer.state.LevelRenderState;
+//$$ import net.minecraft.client.renderer.entity.state.EntityRenderState;
+//#endif
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin
@@ -60,6 +69,14 @@ public class LevelRendererMixin
 
 
   // Everything below this point is yoinked from carpet
+
+  //#if MC < 12104
+  @WrapOperation(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/blockentity/BlockEntityRenderDispatcher;render(Lnet/minecraft/world/level/block/entity/BlockEntity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V"))
+  private void modifyBlockEntityDelta(BlockEntityRenderDispatcher instance, BlockEntity blockEntity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, Operation<Void> original) {
+    if (ClientTickHandler.frozen) f = 1.0f;
+    original.call(instance, blockEntity, f, poseStack, multiBufferSource);
+  }
+  //#endif
 
   //#if MC < 12002
   @Shadow @Final private Minecraft minecraft;
@@ -100,16 +117,24 @@ public class LevelRendererMixin
   //$$ private void modifyDelta(LevelRenderer instance, Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers, Operation<Void> original) {
   //$$     tickDelta = shouldUsePausedDelta(entity) ? 1.0F : tickDelta;
   //$$     original.call(instance, entity, cameraX, cameraY, cameraZ, tickDelta, matrices, vertexConsumers);
-  //$$}
+  //$$ }
+  //$$ @WrapOperation(method = "renderBlockEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/blockentity/BlockEntityRenderDispatcher;render(Lnet/minecraft/world/level/block/entity/BlockEntity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V"))
+  //$$ private void modifyBlockEntityDelta(BlockEntityRenderDispatcher instance, BlockEntity blockEntity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, Operation<Void> original) {
+  //$$   if (ClientTickHandler.frozen) f = 1.0f;
+  //$$   original.call(instance, blockEntity, f, poseStack, multiBufferSource);
+  //$$ }
   //#elseif MC <= 12111
-  //$$ @ModifyArgs(method = "extractEntity", at = @At(                value = "INVOKE",
-  //$$    target = "Lnet/minecraft/client/renderer/entity/EntityRenderDispatcher;extractEntity(Lnet/minecraft/world/entity/Entity;F)Lnet/minecraft/client/renderer/entity/state/EntityRenderState;"
+  //$$ @WrapOperation(method = "extractVisibleEntities", at = @At(value = "INVOKE",
+  //$$    target = "Lnet/minecraft/client/renderer/LevelRenderer;extractEntity(Lnet/minecraft/world/entity/Entity;F)Lnet/minecraft/client/renderer/entity/state/EntityRenderState;"
   //$$ ))
-  //$$ public void modifyDelta(org.spongepowered.asm.mixin.injection.invoke.arg.Args args) {
-  //$$   Entity entity = args.get(0);
-  //$$   float tickDelta = args.get(1);
-  //$$   tickDelta = shouldUsePausedDelta(entity) ? 1.0F : tickDelta;
-  //$$   args.set(1, tickDelta);
+  //$$ private EntityRenderState modifyDelta(LevelRenderer instance, Entity entity, float v, Operation<EntityRenderState> original) {
+  //$$   if (shouldUsePausedDelta(entity)) v = 1.0f;
+  //$$   return original.call(instance, entity, v);
+  //$$ }
+  //$$ @WrapOperation(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;extractVisibleBlockEntities(Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/state/LevelRenderState;)V"))
+  //$$ private void modifyBlockEntityDelta(LevelRenderer instance, Camera camera, float v, LevelRenderState levelRenderState, Operation<Void> original) {
+  //$$   if (ClientTickHandler.frozen) v = 1.0f;
+  //$$   original.call(instance, camera, v, levelRenderState);
   //$$ }
   //#endif
   //#if MC >= 12002
