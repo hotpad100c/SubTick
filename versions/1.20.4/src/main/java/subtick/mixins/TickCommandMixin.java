5fd83952379b9e488c46a0559a950baadfabc379
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-07 20:25:23 +0800

    修复Carpet TIS Addition的tickFreezeCommandToggleable规则和Subtick不兼容的问题

--- versions/1.20.4/src/main/java/subtick/mixins/TickCommandMixin.java
+++ versions/1.20.4/src/main/java/subtick/mixins/TickCommandMixin.java
@@ -1,5 +1,8 @@
 package subtick.mixins;
 
+import carpet.CarpetServer;
+import carpet.api.settings.CarpetRule;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -8,8 +11,6 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.commands.arguments.TimeArgument;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.ServerTickRateManager;
 import net.minecraft.server.commands.TickCommand;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,10 +22,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import subtick.ITickHandler;
 import subtick.Settings;
 import subtick.TickPhase;
-import subtick.util.Translations;
 
 @Debug(export = true)
-@Mixin(value = TickCommand.class, priority = 990)
+@Mixin(value = TickCommand.class)
 public class TickCommandMixin
 {
     @ModifyArg(method = "register", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/CommandDispatcher;register(Lcom/mojang/brigadier/builder/LiteralArgumentBuilder;)Lcom/mojang/brigadier/tree/LiteralCommandNode;"), index = 0)
@@ -57,10 +57,19 @@ public class TickCommandMixin
         cir.setReturnValue(ITickHandler.get(c).when(c));
     }
 
-    @Inject(method = "setFreeze", at = @At("HEAD"), cancellable = true)
-    private static void setFreeze(CommandSourceStack c, boolean freeze, CallbackInfoReturnable<Integer> cir) throws CommandSyntaxException
+    @Inject(method = "setFreeze", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/ServerTickRateManager;setFrozen(Z)V"), cancellable = true)
+    private static void setFreeze(CommandSourceStack c, boolean freeze, CallbackInfoReturnable<Integer> cir, @Local(argsOnly = true) boolean bl) throws CommandSyntaxException
     {
-        if(freeze) {
+        if (Boolean.TRUE.equals(getCarpetRulesValue("tickFreezeCommandToggleable")) ||
+                Boolean.TRUE.equals(getCarpetRulesValue("tickCommandCarpetfied"))) {
+            if (ITickHandler.get(c).frozen()) {
+                cir.setReturnValue(ITickHandler.get(c).unfreeze(c));
+            } else {
+                cir.setReturnValue(ITickHandler.get(c).freeze(c, TickPhase.byCommandKey(Settings.subtickDefaultPhase)));
+            }
+            return;
+        }
+        if(bl) {
             cir.setReturnValue(ITickHandler.get(c).freeze(c, TickPhase.byCommandKey(Settings.subtickDefaultPhase)));
         } else {
             int result = 0;
@@ -69,6 +78,15 @@ public class TickCommandMixin
             }
             cir.setReturnValue(result);
         }
+    }
+
+    @Unique
+    private static Object getCarpetRulesValue(String ruleName) {
+        CarpetRule<?> carpetRule = CarpetServer.settingsManager.getCarpetRule(ruleName);
+        if (carpetRule == null) {
+            return false;
+        }
+        return carpetRule.value() == null ? false : carpetRule.value();
     }
 
     @Inject(method = "step", at = @At("HEAD"), cancellable = true)
